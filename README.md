00a886584e6a96959295a30e5bb9154e864c43be
tomorrow anan <tomorrowanan@gmail.com>
2017-04-09 12:29:55 +0900

    - README更新 - 関数の引数を修正

--- README.md
+++ README.md
@@ -1,5 +1,12 @@
 # golang_slack_bot
 
 # How To Use
-1.
-2.
+
+```golang
+import golang_slack_bot
+
+client := createSlackClient()
+
+client.setPostParamter(bot_name, icon_url, channel)
+response, err := sp.sendMessage( "hello world", "{your incomming webhook url}" )
+```
--- main.go
+++ main.go
@@ -24,12 +24,11 @@ func main() {
 
 	sp := createSlackClient()
 	sp.setPostParamter(
-		"hogehoge",
 		"test",
 		"https://applets.imgix.net/https%3A%2F%2Fassets.ifttt.com%2Fimages%2Fchannels%2F2107379463%2Ficons%2Fon_color_large.png%3Fversion%3D0?ixlib=rails-2.1.3&w=240&h=240&auto=compress&s=07c1117d9e046c1a26150728429d62db",
 		"#memo",
 	)
-	response, err := sp.sendMessage("")
+	response, err := sp.sendMessage("Hello World", "")
 	if err != nil {
 		log.Println(err)
 	}
@@ -41,8 +40,7 @@ func createSlackClient() *Slack {
 	return sp
 }
 
-func (sp *Slack) setPostParamter(msg string, bot_name string, icon_url string, channel string) {
-	sp.Text = msg
+func (sp *Slack) setPostParamter(bot_name string, icon_url string, channel string) {
 	sp.Username = bot_name
 	if icon_url == "" {
 		sp.IconEmoji = ":japanese_goblin:"
@@ -52,13 +50,15 @@ func (sp *Slack) setPostParamter(msg string, bot_name string, icon_url string, c
 	sp.Channel = channel
 }
 
-func (sp *Slack) sendMessage(incomming_webhook_url string) (string, error) {
+func (sp *Slack) sendMessage(msg string, incomming_webhook_url string) (string, error) {
+	sp.Text = msg
+
 	params, err := json.Marshal(*sp)
 	if err != nil {
 		return "", err
 	}
 
-	// config.IncomingUrl を slackのincomming webhookのurlを設定
+	// config.IncomingUrlをslackのincomming webhookのurlに設定
 	if incomming_webhook_url == "" {
 		incomming_webhook_url = config.IncomingUrl
 	}
