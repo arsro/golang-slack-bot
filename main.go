377001ba80993b0bd63642421e6a36d29c47d30d
tomorrow anan <tomorrowanan@gmail.com>
2017-04-09 12:48:05 +0900

    一部しゅうせい

--- main.go
+++ main.go
@@ -6,11 +6,7 @@ import (
 	"log"
 	"net/http"
 	"net/url"
-
-	"golang_slack_bot/config"
 )
-
-const ()
 
 type Slack struct {
 	Text      string `json:"text"`
@@ -18,21 +14,6 @@ type Slack struct {
 	IconEmoji string `json:"icon_emoji"`
 	IconUrl   string `json:"icon_url"`
 	Channel   string `json:"channel"`
-}
-
-func main() {
-
-	sp := createSlackClient()
-	sp.setPostParamter(
-		"test",
-		"https://applets.imgix.net/https%3A%2F%2Fassets.ifttt.com%2Fimages%2Fchannels%2F2107379463%2Ficons%2Fon_color_large.png%3Fversion%3D0?ixlib=rails-2.1.3&w=240&h=240&auto=compress&s=07c1117d9e046c1a26150728429d62db",
-		"#memo",
-	)
-	response, err := sp.sendMessage("Hello World", "")
-	if err != nil {
-		log.Println(err)
-	}
-	log.Println(response)
 }
 
 func createSlackClient() *Slack {
@@ -58,10 +39,6 @@ func (sp *Slack) sendMessage(msg string, incomming_webhook_url string) (string, 
 		return "", err
 	}
 
-	// config.IncomingUrlをslackのincomming webhookのurlに設定
-	if incomming_webhook_url == "" {
-		incomming_webhook_url = config.IncomingUrl
-	}
 	payload := string(string(params))
 	post_value := url.Values{"payload": {payload}}
 
